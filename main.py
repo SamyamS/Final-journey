4d9525722ca3d7eaa27842f6d0b17474418fdafa
samyam91011 <shrestsam@gmail.com>
2023-01-16 00:59:09 -0600

    Added static health bars in bottom panel

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 font = pygame.font.SysFont('Time New Roman', 35)
 red = (255,0,0)
 green = (0,255,0)
+white = (255,255,255)
 
 #just setting the title of the window
 pygame.display.set_caption('Final x Journey')
@@ -42,6 +43,10 @@ def draw_panel():
     screen.blit(panel_img, (0,screen_height - bottom_panel))
     #display knight stats
     draw_text(f'{knight.name} HP:{knight.hp}', font, red, 100, screen_height - bottom_panel + 10 )
+    gap_between_lines = 0
+    for count, i in enumerate(skeleton_list):        
+        draw_text(f'{i.name} {count+1} HP:{i.hp}', font, red, 500, screen_height - bottom_panel + 10 + gap_between_lines)
+        gap_between_lines +=60
 
 
 #Classes TODO: maybe add child classes to solve the animation cutting in half due to variable number of images for each character and their actions
@@ -96,14 +101,38 @@ class character():
         if (self.frame >= len(self.animation_list[self.action])):
             self.frame = 0
 
+#Class for the health bar
+class health_bar: 
+    def __init__(self, x, y, hp, max_hp):
+        self.x = x
+        self.y = y
+        self.hp = hp   #current hp
+        self.max_hp = max_hp
+        self.health_bar_length = 200
+        self.health_ratio = self.max_hp/self.health_bar_length
+        self.health_change_speed = 5
+
+    def draw(self,hp):
+        pygame.draw.rect(screen, red, (self.x,self.y, self.hp/self.health_ratio, 20))
+        pygame.draw.rect(screen, white, (self.x,self.y, self.health_bar_length, 20),4)   #here 4 is the width of the border   
+
+#create objects from the classes here
 knight = character(200,300,'Knight',30,5,3)
 skeleton1 = character(500,400,'Skeleton',10,2,1)
 skeleton2 = character(600,400,'Skeleton',10,2,1)
 
 #make a list to store the skeletons/villains
-villain_list = []
-villain_list.append(skeleton1)
-villain_list.append(skeleton2)
+skeleton_list = []
+skeleton_list.append(skeleton1)
+skeleton_list.append(skeleton2)
+
+# skeleton1.hp = 1 #test to see if the health is decreasing in the health bar
+
+#health bar instances
+knight_health_bar = health_bar(100, screen_height - bottom_panel + 40, knight.hp, knight.max_hp)
+skeleton1_health_bar = health_bar(500, screen_height - bottom_panel + 40, skeleton1.hp, skeleton1.max_hp)
+skeleton2_health_bar = health_bar(500, screen_height - bottom_panel + 100, skeleton2.hp, skeleton2.max_hp)
+
 
 #have to set run as true so pygame can use a while to loop through it 
 run = True
@@ -112,14 +141,19 @@ while run:
     #fix constant fps 
     clock.tick(fps)  #equivalent to pygame.time.Clock().tick(60)   
 
-    #draw the background and panel
+    #draw the background 
     draw_bg()
-    draw_panel()
+
+    #Draw the bottom panels and the health bars in them
+    draw_panel() #draw panel contains draw texts which displays the texts
+    knight_health_bar.draw(knight.hp)
+    skeleton1_health_bar.draw(skeleton1.hp)
+    skeleton2_health_bar.draw(skeleton2.hp)
 
     #draw our hero knight and other characters (skeleton used as the other character for now)
     knight.update_animation()
     knight.draw()
-    for villain in villain_list:
+    for villain in skeleton_list:
         villain.update_animation()
         villain.draw()
 
